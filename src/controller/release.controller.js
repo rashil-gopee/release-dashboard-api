14f98840675919e11406ef1e8949cd3deb7d368f
Isswarraj Gopee <isswarraj.gopee@gmail.com>
2019-04-10 23:30:17 +1000

    Queries

--- src/controller/release.controller.js
+++ src/controller/release.controller.js
@@ -8,7 +8,7 @@ mongoose.Promise = global.Promise;
 
 var JiraClient = require('jira-connector');
 
-var getRelease = function(req, res, next) {
+var getRelease = function (req, res, next) {
 	var jira = new JiraClient({
 		host: 'releasedashboard.atlassian.net',
 		basic_auth: {
@@ -37,40 +37,64 @@ var getRelease = function(req, res, next) {
 	// 	}
 	// );
 
-	let query = {
-		jql: 'project = 10001 & fixVersion = AprilOOCR',
-		maxResults: 100,
-		startAt: 0,
-		fields: ['summary', 'description'],
-		fieldsByKeys: false
-	};
+	// let query = {
+	// 	jql: 'project = 10001 & fixVersion = AprilOOCR',
+	// 	maxResults: 100,
+	// 	startAt: 0,
+	// 	fields: ['summary', 'description'],
+	// 	fieldsByKeys: false
+	// };
 
-	jira.search.search(query, (err, issues) => {
-		console.log(issues);
-	});
+	// jira.search.search(query, (err, issues) => {
+	// 	console.log(issues);
+	// });
 
-	jira.project.getAllProjects({}, function(error, projects) {
-		console.log('error', error);
-		console.log('projects', projects);
-		projects.forEach(project => {
-			console.log('Project Id', project.id);
-			jira.project.getVersions({ projectIdOrKey: project.id }, function(
-				error,
-				versions
-			) {
-				console.log('error', error);
-				console.log('versions', versions);
-				versions.forEach(version => {
-					jira.version.getUnresolvedIssueCount(
-						{ versionId: version.id },
-						function(error, issues) {
-							console.log('issues', issues);
-						}
-					);
-				});
-			});
+	let object;
+
+	jira.project.getAllProjects({}, function (error, projects) {
+		// console.log('error', error);
+		// console.log('projects', projects);
+		// object = JSON.parse(JSON.stringify(projects));
+		object = projects.map(project => {
+			// console.log('Project Id', project.id);
+			jira.project.getVersions(
+				{ projectIdOrKey: project.id },
+				function (error, versions) {
+					// console.log('error', error);
+
+					project.versions = JSON.parse(JSON.stringify(versions));
+
+					project.versions.map(version => {
+						let query = {
+							jql: 'project = ' + project.id + ' & fixVersion = ' + version.name,
+							maxResults: 100,
+							startAt: 0,
+							fields: ['summary', 'description'],
+							fieldsByKeys: false
+						};
+
+						jira.search.search(query, (err, issues) => {
+							// console.log(issues);
+							version.issues = JSON.parse(JSON.stringify(issues));
+							return version;
+						});
+
+						// jira.version.getUnresolvedIssueCount(
+						// 	{ versionId: version.id },
+						// 	function(error, issues) {
+						// 		console.log('issues', issues);
+						// 	}
+						// );
+					});
+				}
+			);
+			// console.log('project', projectVersions);
+			return project;
 		});
-		next();
+		// console.log('object', object);
+		// console.log('send');
+		res.send(object);
+		// next();
 	});
 };
 
