b9b29275b3259eae7b03c62c7157f40d88f61efd
Abhinay Kathuria <abhinay.kathuria@gmail.com>
2019-04-22 18:58:32 +1000

    Release Model Changes

--- src/model/release.model.js
+++ src/model/release.model.js
@@ -7,6 +7,54 @@ var releaseSchema = new Schema({
 		type: Date,
 		required: true
 	},
+	startDate: {
+		type: Date,
+		required: true
+	},
+	devfinish: {
+		type: Date,
+		required: true
+	},
+	regressionDeploy: {
+		type: Date,
+		required: false
+	},
+	refreshDate: {
+		type: Date,
+		required: true
+	},
+	regressionStart: {
+		type: Date,
+		required: true
+	},
+	regressionEnd: {
+		type: Date,
+		required: true
+	},
+	cabDate: {
+		type: Date,
+		required: false
+	},
+	testenvironment: {
+		type: String,
+		required: true
+	},
+	regenvironment: {
+		type: String,
+		required: true
+	},
+	biztalk: {
+		type: String,
+		required: true
+	},
+	sitecore: {
+		type: String,
+		required: true
+	},
+	devsupport: {
+		type: String,
+		required: true
+	},
 	projects: [{
 		projectId: {
 			type: String,
