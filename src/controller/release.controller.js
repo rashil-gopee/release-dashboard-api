5dd490b479be327534ec574d6b17c1346292d903
Isswarraj Gopee <isswarraj.gopee@gmail.com>
2019-05-17 13:13:28 +1000

    Email Notif

--- src/controller/release.controller.js
+++ src/controller/release.controller.js
@@ -2,9 +2,52 @@
 
 const utils = require('../utils');
 const async = require('async');
+const model = require('../model');
 
 var editRelease = function (req, res, next) {
 	// console.log('req', req);
+
+	model.release.findById(req.body._id, (err, release) => {
+		console.log('release', release);
+		// req.body.checklists.forEach(checklist => {
+
+		// });
+		for (var i = 0; i < req.body.checklists.length; i++) {
+			if (req.body.checklists[i].value != release.checklists[i].value) {
+
+				console.log('req.body.checklists[i].contactPerson', req.body.checklists[i].contactPerson);
+				model.user.findById(req.body.checklists[i].contactPerson, (err, user) => {
+					// console.log('user', user);
+					utils.jira.createJiraClient(req, function () {
+						utils.jira.getJiraClient().user.getUser({ username: user.jiraUsername }, function (error, response) {
+							console.log('response', response);
+
+							var subject = 'Reset Password';
+							var html =
+								'Checlist has been checked | unchecked.\n\n';
+
+							utils.sendmail(response.emailAddress, subject, null, html, function (err, sent) {
+								if (err) return res.sendStatus(400);
+							});
+							// utils.sendmail()
+							// next();
+						});
+					});
+				});
+
+				// utils.jira.createJiraClient(req, function () {
+				// 	utils.jira.getJiraClient().user.getUser({ username: req.erm.result.jiraUsername }, function (error, response) {
+				// 		for (var k in response) {
+				// 			req.erm.result[k] = response[k];
+				// 		}
+				// 		next();
+				// 	});
+				// });
+
+			}
+		}
+	});
+
 	next();
 };
 
